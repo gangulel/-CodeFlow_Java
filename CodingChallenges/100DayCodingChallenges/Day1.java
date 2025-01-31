1f361cb691b2be93a646b40a5983978640d851e0
Dishma Gangulel <120539966+gangulel@users.noreply.github.com>
2025-01-31 22:15:08 +0530

    init

---
+++ CodingChallenges/100DayCodingChallenges/Day1.java
@@ -0,0 +1,14 @@
+//Day 1 - Hello World
+/*1. Write a program that prints "Hello World!" to the console
+2. Add single line and multi-line comments to your code
+
+Reading List:
+1. Learn the difference between Java and Python
+2. What is a Java compiler and how does it work?
+3. Difference between Compiler and Interpreter*/
+
+public class Day1 {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
